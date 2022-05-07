68d4dc4d478dbe23b883410de46e6c7f5fc78d61
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-05-07 08:32:12 +0600

    bug fix

--- src/component/othersFile/firebase.init.js
+++ src/component/othersFile/firebase.init.js
@@ -7,23 +7,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_apiKey,
-//     authDomain: process.env.REACT_APP_authDomain,
-//     projectId: process.env.REACT_APP_projectId,
-//     storageBucket: process.env.REACT_APP_storageBucket,
-//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//     appId: process.env.REACT_APP_appId,
-// };
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBDTFmnTqlboBO7rOgFg4f75brVXY1Ftzs",
-    authDomain: "assignment-11-c2cc1.firebaseapp.com",
-    projectId: "assignment-11-c2cc1",
-    storageBucket: "assignment-11-c2cc1.appspot.com",
-    messagingSenderId: "171906770605",
-    appId: "1:171906770605:web:8ea4781c1df9ddbef94824",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
